c4966a20c14bfcea85f999cbf01f8fe2b527c69d
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2026-01-16 14:42:45 -0500

    added logging and supabase sorage path utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const SUPABASE_STORAGE_BUCKETS = {
+  VIDEOS: 'tmp_videos',
+} as const;
+
+export function getVideoStoragePath(videoId: string) {
+  return `videos/${videoId}.mp4`;
+}
+
+export function getAudioStoragePath(videoId: string) {
+  return `audios/${videoId}.mp3`;
+}
